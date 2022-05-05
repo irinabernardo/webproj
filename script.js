4b75f7041bbe893495f8bc1679853a71796a9d90
irinabernardo <irinakn@outlook.com>
2022-05-05 21:09:11 +0100

    Col fade in index demo
    Col on index page slide in with. No background on rows

---
+++ script.js
@@ -0,0 +1,21 @@
+document.getElementById("pageRows").style.visibility = "hidden";
+window.onscroll = function() {myFunction()};
+
+let leftCol = document.getElementsByClassName("row-img-left");
+let rightCol = document.getElementsByClassName("row-img-right");
+
+var trigger = document.getElementById("intro").offsetTop;
+
+
+function myFunction() {
+    if (window.pageYOffset >= trigger) {
+        document.getElementById("pageRows").style.visibility = "visible";
+        
+        for (let i = 0; i < leftCol.length; i++) {     
+    leftCol[i].classList.add("fade-in-left");
+        }
+        for (let i = 0; i < rightCol.length; i++) {     
+            rightCol[i].classList.add("fade-in-right");
+        }  
+}  
+}
